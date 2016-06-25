01b91d0b4d93b345e541853a3d458272363cbe43
wujianguo <lsjustin89@gmail.com>
2016-06-25 22:34:39 +0800

    init

---
+++ SNSKit/SNSKit.h
@@ -0,0 +1,19 @@
+//
+//  SNSKit.h
+//  SNSKit
+//
+//  Created by wujianguo on 16/6/25.
+//  Copyright © 2016年 wujianguo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SNSKit.
+FOUNDATION_EXPORT double SNSKitVersionNumber;
+
+//! Project version string for SNSKit.
+FOUNDATION_EXPORT const unsigned char SNSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNSKit/PublicHeader.h>
+
+
---
+++ Sources/SNSPlatformManager.swift
@@ -0,0 +1,13 @@
+//
+//  SNSPlatformManager.swift
+//  SNSKit
+//
+//  Created by wujianguo on 16/6/25.
+//  Copyright © 2016年 wujianguo. All rights reserved.
+//
+
+import Foundation
+
+class SNSPlatformManager {
+    
+}
